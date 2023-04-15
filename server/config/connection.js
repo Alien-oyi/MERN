bd4c7c5582cb262142590f779011c91a27b09927
Alien-oyi <daijinmin78@gmail.com>
2023-04-14 23:30:05 -0400

    add screenshot

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://184.153.84.61:32/googlebooks');
 
 module.exports = mongoose.connection;
